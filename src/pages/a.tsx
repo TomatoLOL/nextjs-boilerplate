09872b51ae31047befffc7455e58f37a2fcd0996
TomatoLOL <72852060+TomatoLOL@users.noreply.github.com>
2021-02-23 01:00:53 -0600

    Initial commit

---
+++ src/pages/a.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+// eslint-disable-next-line import/no-default-export
+export default function anythine(): JSX.Element {
+	return (
+		<>
+			<a href="https://instagram.com" target="_blank">
+				<Image src="/sexysnakepics/sexysnake.jpg" layout="fill" />
+			</a>
+		</>
+	
+	);
+}
